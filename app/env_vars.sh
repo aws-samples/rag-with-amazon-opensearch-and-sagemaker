1cbe9ba0725408c51b1dcf397da57a77edb438dc
Safa Tinaztepe <saftinaz@88665a4a7e5f.ant.amazon.com>
2024-05-02 11:17:37 -0400

    restore env vars sh

---
+++ app/env_vars.sh
@@ -0,0 +1,6 @@
+export AWS_REGION="your-aws-region"
+export OPENSEARCH_SECRET="your-opensearch-secret"
+export OPENSEARCH_DOMAIN_ENDPOINT="your-opensearch-url"
+export OPENSEARCH_INDEX="llm_rag_embeddings"
+export EMBEDDING_ENDPOINT_NAME="your-sagemaker-endpoint-for-embedding-model"
+export TEXT2TEXT_ENDPOINT_NAME="your-sagemaker-endpoint-for-text-generation-model"
